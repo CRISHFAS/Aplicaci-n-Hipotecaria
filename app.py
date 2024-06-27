94fcbef905ba482d3a0c0c1671a41e77d875e998
Garrett Grolemund <garrett@posit.co>
2024-06-27 16:22:34 -0500

    Lays out app

--- app.py
+++ app.py
@@ -2,8 +2,57 @@
 
 from shiny import reactive
 from shiny.express import render, input, ui
+import faicons as fa
 import helpers
 
 # Set up the UI
-ui.page_opts(fillable=True)
+ui.page_opts(fillable=True, title='Interest Calculator App')
 
+with ui.layout_columns(col_widths=(2, 2, 2, 3, 3, 4, 8, 2, 2)):
+
+    ui.input_numeric("amount", "Amount Financed", 100000, min=1, step=1000) 
+    ui.input_numeric("rate", "Interest Rate", 7.25, min=0.01, step=0.05) 
+    ui.input_numeric("term", "Term of Loan", 30, min=0.25, step=5) 
+
+    @reactive.calc
+    def amortization_df():
+        return helpers.create_amortization_table(input.amount(), input.rate(), input.term())
+
+    @reactive.calc
+    def total_paid():
+        return helpers.calculate_total_paid(amortization_df())
+
+    with ui.value_box(showcase=fa.icon_svg("sack-dollar"), theme="bg-gradient-indigo-purple"):
+        "Total Paid"
+
+        @render.text
+        def total_paid_amount():
+            return f'{total_paid():,}'
+    
+    with ui.value_box(showcase=fa.icon_svg("percent"), theme="bg-gradient-indigo-purple"):
+        "Percent Interest"
+
+        @render.text
+        def percent_interest():
+            return helpers.calculate_percent_interest(input.amount(), total_paid())
+
+    @render.data_frame
+    def payments():
+        return render.DataGrid(
+            helpers.create_payment_schedule_to_edit(amortization_df()), 
+            editable=True
+        )
+
+    with ui.navset_card_underline():  
+        with ui.nav_panel("Cumulative Amount"):
+            @render.plot
+            def cumulative_plot():
+                return helpers.plot_amount_paid_over_time(amortization_df())
+
+        with ui.nav_panel("Payment Composition"):
+            @render.plot
+            def payments_composition_plot():
+                return helpers.plot_payment_composition_over_time(amortization_df())
+
+    ui.input_action_button("update", "Update Payments")
+    ui.input_action_button("download", "Download Schedule")

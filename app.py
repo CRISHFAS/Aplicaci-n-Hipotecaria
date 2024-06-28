8c8d365381c0b282511aea400a153a6e97bbfa3f
Garrett Grolemund <garrett@posit.co>
2024-06-28 17:31:47 -0500

    Adds reactions to table edits

--- app.py
+++ app.py
@@ -5,22 +5,15 @@ from shiny.express import render, input, ui
 import faicons as fa
 import helpers
 
-# Set up the UI
+# Begin UI --------------------------------------------------------------------
 ui.page_opts(fillable=True, title='Interest Calculator App')
 
 with ui.layout_columns(col_widths=(2, 2, 2, 3, 3, 4, 8, 2, 2)):
 
+    # Inputs ----------------------------------------------------------------
     ui.input_numeric("amount", "Amount Financed", 100000, min=1, step=1000) 
     ui.input_numeric("rate", "Interest Rate", 7.25, min=0.01, step=0.05) 
     ui.input_numeric("term", "Term of Loan", 30, min=0.25, step=5) 
-
-    @reactive.calc
-    def amortization_df():
-        return helpers.create_amortization_table(input.amount(), input.rate(), input.term())
-
-    @reactive.calc
-    def total_paid():
-        return helpers.calculate_total_paid(amortization_df())
 
     with ui.value_box(showcase=fa.icon_svg("sack-dollar"), theme="bg-gradient-indigo-purple"):
         "Total Paid"
@@ -36,13 +29,35 @@ with ui.layout_columns(col_widths=(2, 2, 2, 3, 3, 4, 8, 2, 2)):
         def percent_interest():
             return helpers.calculate_percent_interest(input.amount(), total_paid())
 
+    # Editable Table ----------------------------------------------------------------
+    amortization_df = reactive.value()
+
+    @reactive.effect
+    def _():
+        amortization_df.set(
+            helpers.make_amortization_table(input.amount(), input.rate(), input.term())
+        )
+
     @render.data_frame
     def payments():
         return render.DataGrid(
-            helpers.create_payment_schedule_to_edit(amortization_df()), 
+            helpers.make_payment_schedule(amortization_df()), 
             editable=True
         )
+    
+    @payments.set_patch_fn
+    def upgrade_patch(*, patch,):
+        if patch["column_index"] == 1:
+            return helpers.cell_to_float(patch["value"])
+        else:
+            return patch["value"]
 
+    # Reactive Calcs ----------------------------------------------------------------
+    @reactive.calc
+    def total_paid():
+        return helpers.calculate_total_paid(amortization_df())
+
+    # Plots ----------------------------------------------------------------
     with ui.navset_card_underline():  
         with ui.nav_panel("Cumulative Amount"):
             @render.plot
@@ -56,3 +71,17 @@ with ui.layout_columns(col_widths=(2, 2, 2, 3, 3, 4, 8, 2, 2)):
 
     ui.input_action_button("update", "Update Payments")
     ui.input_action_button("download", "Download Schedule")
+
+# Update payments schedule -----------------------------------------------------
+@reactive.effect
+@reactive.event(input.update)
+def _():
+    updated_schedule = helpers.make_amortization_table(
+        input.amount(), 
+        input.rate(), 
+        input.term(),
+        payments.data_view()['Amount'],
+        payments.data_view()['Notes']
+    )
+    amortization_df.set(updated_schedule)
+    return None
--- helpers.py
+++ helpers.py
@@ -2,15 +2,23 @@
 import pandas as pd
 import plotnine as p9
 
-def create_amortization_table(amount, rate, term):
-    # Calculate monthly interest rate
-    monthly_interest_rate = rate / 12 / 100
-    
+
+def make_amortization_table(amount, rate, term, payments = None, notes = None):
+
     # Calculate total number of payments
-    total_payments = term * 12
-    
-    # Calculate Amount using the formula for an annuity
-    monthly_payment = amount * (monthly_interest_rate * (1 + monthly_interest_rate) ** total_payments) / ((1 + monthly_interest_rate) ** total_payments - 1)
+    n_payments = term * 12
+
+    # Calculate monthly interest rate
+    monthly_rate = rate / 12 / 100
+
+    # Assemble payments schedule
+    if payments is None:
+        monthly_payment = amount * (monthly_rate * (1 + monthly_rate) ** n_payments) / ((1 + monthly_rate) ** n_payments - 1)
+        payments = [monthly_payment] * n_payments
+
+    # Assemble notes field
+    if notes is None:
+        notes = [''] * n_payments
     
     # Initialize amortization table
     amortization_table = []
@@ -19,21 +27,36 @@ def create_amortization_table(amount, rate, term):
     remaining_balance = amount
     
     # Calculate each month's principal and interest
-    for payment_number in range(1, total_payments + 1):
-        interest_payment = remaining_balance * monthly_interest_rate
-        principal_payment = monthly_payment - interest_payment
-        remaining_balance -= principal_payment
-        amortization_table.append([payment_number, monthly_payment, principal_payment, interest_payment, remaining_balance])
+    for payment_number in range(1, n_payments + 1):
+        if remaining_balance == 0:
+            amortization_table.append([payment_number, 0, 0, 0, 0, notes[payment_number - 1]])
+        elif payment_number == (n_payments):
+            interest_payment = remaining_balance * monthly_rate
+            payment = remaining_balance + interest_payment
+            principal_payment = payment - interest_payment
+            remaining_balance = max(0, remaining_balance - principal_payment)
+            amortization_table.append([payment_number, payment, principal_payment, interest_payment, remaining_balance, notes[payment_number - 1]])
+        elif payments[payment_number - 1] == 0:
+            interest_payment = remaining_balance * monthly_rate
+            remaining_balance += interest_payment
+            amortization_table.append([payment_number, 0, 0, 0, remaining_balance, notes[payment_number - 1]])
+        else:
+            interest_payment = remaining_balance * monthly_rate
+            payment = payments[payment_number - 1]
+            principal_payment = payment - interest_payment
+            remaining_balance = max(0, remaining_balance - principal_payment)
+            amortization_table.append([payment_number, payment, principal_payment, interest_payment, remaining_balance, notes[payment_number - 1]])
 
     # Create DataFrame for better visualization
-    amortization_df = pd.DataFrame(amortization_table, columns=['Payment', 'Amount', 'Principal Payment', 'Interest Payment', 'Remaining Balance'])
+    amortization_df = pd.DataFrame(
+        amortization_table, 
+        columns=['Payment', 'Amount', 'Principal Payment', 'Interest Payment', 'Remaining Balance', 'Notes']
+    ).round(2)
     
     return amortization_df
 
-def create_payment_schedule_to_edit(amortization_table):
-    payment_schedule = amortization_table[['Payment', 'Amount']].round(2)
-    payment_schedule['Notes'] = ""
-    return payment_schedule
+def make_payment_schedule(amortization_table):
+    return amortization_table[['Payment', 'Amount', 'Notes']]
 
 def calculate_total_paid(amortization_table):
     return amortization_table['Amount'].sum().round(2)
@@ -66,3 +89,13 @@ def plot_payment_composition_over_time(amortization_table):
         + p9.theme_linedraw()
         + p9.theme(legend_position='top', legend_direction='horizontal', legend_title=p9.element_blank())
     )
+
+def cell_to_float(s):
+    try:
+        result = float(s)
+    except ValueError:
+        raise SafeException(
+                "Amount values should be numbers."
+            )
+    else:
+        return result
